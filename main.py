cf9184172f8b5ec4ce3a62f9341a9cbe09950ca8
goo-dness <goodnessakuba1708@gmail.com>
2026-05-04 20:11:55 +0100

    fix startup- remove create_all and fix cors

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from dotenv import load_dotenv
+
+load_dotenv()
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -9,9 +11,6 @@ from routes.messier_objects import router as messier_router
 from routes.planets import router as planet_router
 from routes.stars import router as star_router
 
-load_dotenv()
-Base.metadata.create_all(bind=engine)
-
 app = FastAPI(
     title="Space Catalog",
     description="A space education knowledge platform",
@@ -20,10 +19,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5500",
-        "http://127.0.0.1:5500",
-    ],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
