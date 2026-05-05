00bd047b150599393ead93cea16650a28650d0c9
goo-dness <goodnessakuba1708@gmail.com>
2026-05-05 22:12:48 +0100

    update database.py

--- core/database.py
+++ core/database.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 load_dotenv()
-DATABASE_URL = os.environ.get("DB_CONNECTION_URL")
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 
 class Base(DeclarativeBase):
