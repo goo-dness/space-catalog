930f393d1134f557da3f5295742e974fe89a5a9c
goo-dness <goodnessakuba1708@gmail.com>
2026-05-05 05:41:02 +0100

    add bind engine

--- core/database.py
+++ core/database.py
@@ -1,7 +1,8 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
 import os
+
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
@@ -13,6 +14,7 @@ class Base(DeclarativeBase):
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base.metadata.create_all(bind=engine)
 
 
 def get_db():
