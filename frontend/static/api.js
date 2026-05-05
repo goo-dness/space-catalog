200cf4324da892a884c8955de6eb3a58344b3623
goo-dness <goodnessakuba1708@gmail.com>
2026-05-05 23:44:34 +0100

    Update api.js

--- frontend/static/api.js
+++ frontend/static/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://space-catalog-production.up.railway.app";
+const API_BASE = "https://space-catalog-production.up.railway.app/api/v1";
 
 async function fetchAPOD() {
   const response = await fetch(`${API_BASE}/apod`);
