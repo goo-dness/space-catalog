449061bb78f9087bda2c7332298a27eac957ef8c
goo-dness <goodnessakuba1708@gmail.com>
2026-05-07 09:46:07 +0100

    update main.js

--- frontend/static/main.js
+++ frontend/static/main.js
@@ -21,69 +21,95 @@ document.addEventListener("DOMContentLoaded", async () => {
         <p class="apod-explanation">${apod.explanation}</p>
       </div>
     `;
-  } else if (apod.media_type === "video") {
-    apodContainer.innerHTML = `
+  } else if (apod.media_type === "video" && apod.url) {
+    const isYoutube =
+      apod.url.includes("youtube.com") || apod.url.includes("youtu.be");
+
+    if (isYoutube) {
+      const embedUrl = apod.url.replace("watch?v=", "embed/");
+
+      apodContainer.innerHTML = `
         <div class="apod-video-wrapper">
           <iframe
-            src="${apod.url}"
+            src="${embedUrl}"
             title="${apod.title}"
             frameborder="0"
             allowfullscreen
             class="apod-video">
           </iframe>
         </div>
+
         <div class="apod-info">
           <h3 class="apod-title">${apod.title}</h3>
           <p class="apod-date">${apod.date}</p>
           <p class="apod-explanation">${apod.explanation}</p>
         </div>
       `;
+    } else {
+      apodContainer.innerHTML = `
+        <div class="apod-info">
+          <h3 class="apod-title">${apod.title}</h3>
+          <p class="apod-date">${apod.date}</p>
+
+          <p>
+            Video cannot be embedded.
+            <a href="${apod.url}" target="_blank">
+              Watch here
+            </a>
+          </p>
+
+          <p class="apod-explanation">
+            ${apod.explanation}
+          </p>
+        </div>
+      `;
+    }
   }
-});
 
-const counters = {
-  "star-count": stars.length,
-  "planet-count": planets.length,
-  "agency-count": agencies.length,
-  "messier-count": messier.length,
-};
+  const counters = {
+    "star-count": stars.length,
+    "planet-count": planets.length,
+    "agency-count": agencies.length,
+    "messier-count": messier.length,
+  };
 
-Object.entries(counters).forEach(([id, value]) => {
-  document.getElementById(id).textContent = value;
-});
-
-const featuredCards = document.getElementById("featured-cards");
-const allObjects = [
-  ...stars.map((s) => ({ ...s, type: "Star" })),
-  ...planets.map((p) => ({ ...p, type: "Planet" })),
-  ...messier.map((m) => ({ ...m, type: "Messier Object" })),
-];
-const today = new Date();
-const daySeed = today.getDate() + today.getMonth() + today.getFullYear();
-
-const startIndex = daySeed % allObjects.length;
-const featured = allObjects.slice(startIndex, startIndex + 3);
-
-featured.forEach((obj) => {
-  const card = document.createElement("div");
-  card.className = "card";
-  card.style.cursor = "pointer";
-  card.addEventListener("click", () => {
-    if (obj.type === "Star")
-      window.location.href = `star-detail.html?id=${obj.id}`;
-    else if (obj.type === "Planet")
-      window.location.href = `planet-detail.html?id=${obj.id}`;
-    else window.location.href = `messier-detail.html?id=${obj.id}`;
+  Object.entries(counters).forEach(([id, value]) => {
+    document.getElementById(id).textContent = value;
   });
-  card.innerHTML = `
+
+  const featuredCards = document.getElementById("featured-cards");
+  const allObjects = [
+    ...stars.map((s) => ({ ...s, type: "Star" })),
+    ...planets.map((p) => ({ ...p, type: "Planet" })),
+    ...messier.map((m) => ({ ...m, type: "Messier Object" })),
+  ];
+  const today = new Date();
+  const daySeed = today.getDate() + today.getMonth() + today.getFullYear();
+
+  const startIndex = daySeed % allObjects.length;
+  const featured = allObjects.slice(startIndex, startIndex + 3);
+
+  featured.forEach((obj) => {
+    const card = document.createElement("div");
+    card.className = "card";
+    card.style.cursor = "pointer";
+    card.addEventListener("click", () => {
+      if (obj.type === "Star")
+        window.location.href = `star-detail.html?id=${obj.id}`;
+      else if (obj.type === "Planet")
+        window.location.href = `planet-detail.html?id=${obj.id}`;
+      else window.location.href = `messier-detail.html?id=${obj.id}`;
+    });
+    card.innerHTML = `
       <h3>${obj.name}</h3>
       <p class="card-type>${obj.type}</p>
       <p>${obj.description || obj.common_name || "Learn more about this object"}</p>
       <a class="card-link">Learn more &rarr;</a>
       `;
-  featuredCards.appendChild(card);
-});
+    featuredCards.appendChild(card);
+  });
 
-console.log("stars:", stars);
-console.log("planets:", planets);
-console.log("apod:", apod);
+  console.log("stars:", stars);
+  console.log("planets:", planets);
+  console.log("apod:", apod);
+});
