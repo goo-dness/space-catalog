11047a63571e750a6ad8f91cb51e2bc4ce32ef4d
goo-dness <goodnessakuba1708@gmail.com>
2026-05-14 16:13:44 +0100

    Update search.js

--- frontend/static/search.js
+++ frontend/static/search.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch(`/api/v1/search?q=${encodeURIComponent(query)}`)
       .then((response) => {
         if (!response.ok) throw new Error(`${response.status}`);
-        response.json();
+        return response.json();
       })
       .then((data) => {
         resultsContainer.innerHTML = "";
